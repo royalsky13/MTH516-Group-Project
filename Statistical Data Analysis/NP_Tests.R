8de652a121a43c2a5e984ccef08a8ce7c8e2bf32
swapnonilmondal <98679771+swapnonilmondal@users.noreply.github.com>
2024-11-04 19:43:06 +0530

    Update NP_Tests.R

--- Statistical Data Analysis/NP_Tests.R
+++ Statistical Data Analysis/NP_Tests.R
@@ -1,1 +1,17 @@
+# These are the suuplimentary codes, will be used for testing for the residuals
 
+# install.packages("BSDA")  # Only needed once
+library(BSDA)
+
+# Perform one-sample sign test
+SIGN.test(clim.data$ANN- mean(clim.data$ANN), md = 0, alternative = "two.sided")
+
+# install.packages("randtests")  # Run this line only once
+library(randtests)
+
+# Perform two sided run test
+runs.test(clim.data$ANN)
+
+# Perform one-sided run tests
+runs.test(clim.data$ANN, alternative = "left.sided")
+runs.test(clim.data$ANN, alternative = "right.sided")
